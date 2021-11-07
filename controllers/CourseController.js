7023c157bb397c55c11ca06bf6609e52750b615a
Stewart Newnham <git@newnham.com>
2021-11-07 19:13:40 +0800

    Set up authorization checks for courses

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -7,8 +7,6 @@ const queryHandler = require('../middleware/queryHandler');
 const CourseModel = require('../models/Course');
 
 
-
-
 // @desc: Get All Courses with Query and Pagination functionality
 // @route: GET /api/v1/courses
 // @route: GET /api/v1/bootcamps/:bootcampId/courses
@@ -46,19 +44,27 @@ const getCourseById = asyncHandler (async (req, res, next) => {
     });
 });
 
-// @desc: POST - Add A Course
+// @desc: POST - Add A Course, but only by the Bootcamp publisher or Admin
 // @route: POST /api/v1/bootcamps/:bootcampId/courses
 // @access: private 
 // Using asyncHandler
 const addCourse = asyncHandler (async (req, res, next) => {
 
+    // Put bootcamp and user id into the req.body
     req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
 
+    // Check if bootcamp exists
     const bootcamp = await Bootcamp.findById(req.params.bootcampId);
-
     if(!bootcamp){
         return next(new ErrorResponse(`No Bootcamp Found with id: ${req.params.bootcampId}`, 404));
     }
+
+    // Make sure the user is the publisher for the bootcamp or is admin
+    if(req.user.id !== bootcamp.user.toString() && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.user.id} does not have authorization to Add a Course to the bootcamp`), 401);
+    }
+
 
     const course = await Course.create(req.body);
 
@@ -80,7 +86,18 @@ const updateCourse = asyncHandler (async (req, res, next) => {
     if(!course){
         return next(new ErrorResponse(`No Course Found with id: ${req.params.id}`, 404));
     }
-   
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+          new ErrorResponse(
+            `User ${req.user.id} is not authorized to update course ${course._id}`,
+            401
+          )
+        );
+    }
+    
+    
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
@@ -102,6 +119,16 @@ const deleteCourse = asyncHandler (async (req, res, next) => {
 
     if(!course){
         return next(new ErrorResponse(`No Course Found with id: ${req.params.id}`, 404));
+    }
+    
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User ${req.user.id} is not authorized to delete course ${course._id}`,
+                401
+            )
+        );
     }
    
     await course.remove();
--- routes/courses.js
+++ routes/courses.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const courseController = require('../controllers/CourseController');
 const queryHandler = require('../middleware/queryHandler');
+const authHandler = require('../middleware/authHandler');
 const CourseModel = require('../models/Course');
 
 const router = express.Router({mergeParams: true}); // merge Routes for redirection
 
 router.route('/').get(queryHandler(CourseModel, {path: 'bootcamp', select: 'name description'}), courseController.getCourses)
-                 .post(courseController.addCourse);
+                 .post(authHandler.protectRoute, authHandler.authorizeRoute('publisher', 'admin'), courseController.addCourse);
 
 router.route('/:id').get(courseController.getCourseById)
-                    .put(courseController.updateCourse)
-                    .delete(courseController.deleteCourse);
+                    .put(authHandler.protectRoute, authHandler.authorizeRoute('publisher', 'admin'), courseController.updateCourse)
+                    .delete(authHandler.protectRoute, authHandler.authorizeRoute('publisher', 'admin'), courseController.deleteCourse);
 
 module.exports = router;
